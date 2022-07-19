6847ed6fb3d1f52213ca0dac16a2ea099a70f27c
Israel S. Santos <israel.santos@zukk.com.br>
2022-07-19 12:37:27 -0300

    fix: process env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.spoonacular.com',
   apiUrlRecipes: 'https://api.spoonacular.com/recipes/random?number=12',
-  apiKey: 'b0ad4d6019834327b384f29dd5c3844c'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,4 @@ export const environment = {
   apiUrl: 'https://api.spoonacular.com',
   apiUrlRecipes: 'https://api.spoonacular.com/recipes/random?number=12',
   apiKey: 'b0ad4d6019834327b384f29dd5c3844c'
-};
+}
