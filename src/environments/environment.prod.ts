aa44f55dde1e3ecf2243b5b4847799866d5db9a8
Israel S. Santos <israel.santos@zukk.com.br>
2022-07-19 12:08:07 -0300

    fix: added enviroment to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.spoonacular.com',
+  apiUrlRecipes: 'https://api.spoonacular.com/recipes/random?number=12',
+  apiKey: 'b0ad4d6019834327b384f29dd5c3844c'
 };
